1425c58fcde1be0d0a48ca73aee2bed8d0d69e58
Daisuke Fujita <dtanshi45@gmail.com>
2015-05-26 16:22:44 +0900

    Remove hash rocket in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,4 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'terraforming/dnsimple'
 
 require 'webmock/rspec'
-WebMock.disable_net_connect!(:allow => "codeclimate.com")
+WebMock.disable_net_connect!(allow: "codeclimate.com")
